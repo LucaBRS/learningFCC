2dd652046bc75a64e400e4951c0f9eec9463d7a9
Luca <luca.barsottini@outloo.it>
2024-06-25 23:22:28 +0200

    created methods

--- spring-boot/runnerz/runnerz/src/main/java/dev/lbrs/runnerz/run/RunController.java
+++ spring-boot/runnerz/runnerz/src/main/java/dev/lbrs/runnerz/run/RunController.java
@@ -1,9 +1,12 @@
 package dev.lbrs.runnerz.run;
 
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs") // useful to put at the controller level
@@ -47,36 +50,36 @@ public class RunController {
     }
 
 
-//    // EXAMPLE OF DYNAMIC VARIABLE!!!
-//    @GetMapping("/{id}")
-//    Run findById(@PathVariable Integer id) {
-//        Optional<Run> run = runRepository.findById(id);
-//        if (run.isEmpty()) {
-//            throw  new RunNotFoundExxception();
-//        }
-//        return run.get();
-//    }
-//
-//    //post
-//    @ResponseStatus(HttpStatus.CREATED) // this is a 201 response!
-//    @PostMapping("")
-//    void create(@Valid @RequestBody Run run) {// CHECK THE @Validate it validate the info of the run before the runRepository
-//        System.out.println(run);
-//        runRepository.create(run);
-//    }
-//
-//    //put
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @PutMapping("/{id}")
-//    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
-//        runRepository.update(run, id);
-//
-//    }
-//
-//    //delete
-//    @DeleteMapping("/{id}")
-//    void delete(@PathVariable Integer id) {
-//        runRepository.delete(id);
-//    }
+    // EXAMPLE OF DYNAMIC VARIABLE!!!
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw  new RunNotFoundExxception();
+        }
+        return run.get();
+    }
+
+    //post
+    @ResponseStatus(HttpStatus.CREATED) // this is a 201 response!
+    @PostMapping("")
+    void create(@Valid @RequestBody Run run) {// CHECK THE @Validate it validate the info of the run before the runRepository
+        System.out.println(run);
+        runRepository.create(run);
+    }
+
+    //put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+
+    }
+
+    //delete
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
 
 }
