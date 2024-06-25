74e9ddd199148d66f208d7f05f6a5ee3ccd8041c
Luca <luca.barsottini@outloo.it>
2024-06-25 22:53:33 +0200

    get from a database

--- spring-boot/runnerz/runnerz/src/main/java/dev/lbrs/runnerz/run/RunRepository.java
+++ spring-boot/runnerz/runnerz/src/main/java/dev/lbrs/runnerz/run/RunRepository.java
@@ -1,62 +1,34 @@
 package dev.lbrs.runnerz.run;
 
-import jakarta.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+
 
 @Repository
 public class RunRepository {
 
-    private List<Run> runs = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
+
+    // DEPENDENCY INJECTION!!!!
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
 
     public List<Run> findAll() {
-        return runs;
+        return jdbcClient.sql("select * from run")
+                .query(Run.class)
+                .list();
     }
 
-    Optional<Run> findById(Integer id) {
-        return runs.stream()
-                .filter(run -> run.id() == id)
-                .findFirst()
-                ; //
-    }
-
-    void create(Run run) {
-        runs.add(run);
-    }
-
-    void update(Run run, Integer id) {
-        Optional<Run> existRun = findById(id);
-        existRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
-    }
-
-    void delete(Integer id) { runs.removeIf(run -> run.id().equals(id)); }
-
-    // executed after dependency injection, TO PERFORM ANY INITIALIZATION!!
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-                "morning monday",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
-                3,
-                Location.INDOOR
-        ));
-
-        runs.add(new Run(2,
-                "ddddd monday",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(600, ChronoUnit.MINUTES),
-                3,
-                Location.INDOOR
-        ));
-    }
 
 
 }
---
+++ spring-boot/runnerz/runnerz/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (1,'Morn', '2013-01-01 06:00:00', '2013-01-01 07:00:00', 5, 'OUTDOOR' );
